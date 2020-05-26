bcc957e65603c8e1e09427cac707663575de644c
eduingui <52107695+eduingui@users.noreply.github.com>
2020-05-26 20:56:24 -0300

    Update settings.py

--- MagicHolidays/settings.py
+++ MagicHolidays/settings.py
@@ -140,8 +140,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        "rest_framework_api_key.permissions.HasAPIKey"
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': [
