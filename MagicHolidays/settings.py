7fa2eb140ec19773ce141b7f6c05b31c7be6fe1b
eduingui <52107695+eduingui@users.noreply.github.com>
2020-05-26 21:44:03 -0300

    Update settings.py

--- MagicHolidays/settings.py
+++ MagicHolidays/settings.py
@@ -142,9 +142,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES':  [
-        'rest_framework_api_key.permissions.HasAPIKey'
-    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
