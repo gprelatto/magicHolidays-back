35d4637b023b0369605a46701d287d4f927e71cd
gonzalo <gonzalo@ipsy.com>
2020-06-01 17:56:37 -0300

    cambio nombre de endpoint

--- MagicHolidays/urls.py
+++ MagicHolidays/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('hello/', views.LoginView.as_view(), name='hello')
+    path('login/', views.LoginView.as_view(), name='hello')
 ]
